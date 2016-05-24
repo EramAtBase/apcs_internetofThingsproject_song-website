52a46f158b31b45a965b38a1bfcd48c0010a6a04
Eram Manasia <eram.m@bronxsoftware.org>
2016-05-24 09:37:46 -0400

    Create Song.js
    This is the code for the song we made. Any new changes will be updated.

---
+++ Song.js
@@ -0,0 +1,10 @@
+//Eram and Mariam (IoT project [song])
+
+init(); 
+setTempo(120); 
+fitMedia(YG_TRAP_HIHAT_5, 1, 1, 100);
+fitMedia(YG_RNB_FUNK_SHAKER_2, 2, 1, 100);
+fitMedia(YG_POP_MELODY_5, 3, 1, 100);
+
+
+finish();
